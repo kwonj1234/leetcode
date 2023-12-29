f479fc8aeaccad9b6c6f53475e49266fca517fc9
Jae Kwon <kwonj1234@gmail.com>
2023-12-28 21:22:02 -0600

    sum root to leaf num

---
+++ top_interview_150/Binary Tree General/medium/sum_root_to_leaf_numbers/sum_root_to_leaf_numbers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        leaf_numbers = []
+
+        def find_leaf_numbers(node, current_val):
+            new_val = (current_val * 10) + node.val
+            if node.left is None and node.right is None:
+                leaf_numbers.append(new_val)
+            if node.left is not None:
+                find_leaf_numbers(node.left, new_val)
+            if node.right is not None:
+                find_leaf_numbers(node.right, new_val)
+        
+        find_leaf_numbers(root, 0)
+
+        return sum(leaf_numbers)
